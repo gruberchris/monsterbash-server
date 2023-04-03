9af4b697bc826b0082f391ba905104d80df5e949
Chris Gruber <gruber.christopher@gmail.com>
2023-04-02 23:02:36 -0500

    test hub broadcast and hub processing single recipient and broadcast message channels

--- pkg/ws/hub_test.go
+++ pkg/ws/hub_test.go
@@ -92,3 +92,102 @@ func TestHub_Send(t *testing.T) {
 		t.Errorf("Expected message to be received")
 	}
 }
+
+func TestHub_Broadcast(t *testing.T) {
+	hub := NewHub()
+
+	go hub.Run()
+
+	conn := &websocket.Conn{}
+	client := hub.Register(conn)
+
+	conn2 := &websocket.Conn{}
+	client2 := hub.Register(conn2)
+
+	hub.Broadcast([]byte("test"))
+
+	time.Sleep(10 * time.Millisecond)
+
+	message := <-client.send
+
+	if string(message) != "test" {
+		t.Errorf("Expected Client One message to be 'test', got %v", message)
+	}
+
+	message = nil
+
+	message = <-client2.send
+
+	if string(message) != "test" {
+		t.Errorf("Expected Client Two message to be 'test', got %v", message)
+	}
+}
+
+func TestHub_ProcessSendMessages(t *testing.T) {
+	hub := NewHub()
+
+	go hub.Run()
+
+	conn := &websocket.Conn{}
+	client := hub.Register(conn)
+
+	singleSend := make(chan HubSingleSendMessageEvent, BufferSize)
+
+	singleSend <- HubSingleSendMessageEvent{
+		Message:  []byte("test"),
+		ClientID: client.ID,
+	}
+
+	// Close the channel to avoid a deadlock
+	close(singleSend)
+
+	hub.ProcessSendMessages(singleSend)
+
+	time.Sleep(10 * time.Millisecond)
+
+	select {
+	case message := <-client.send:
+		if string(message) != "test" {
+			t.Errorf("Expected message to be 'test', got %v", message)
+		}
+	}
+}
+
+func TestHub_ProcessBroadcastMessages(t *testing.T) {
+	hub := NewHub()
+
+	go hub.Run()
+
+	conn := &websocket.Conn{}
+	client := hub.Register(conn)
+
+	conn2 := &websocket.Conn{}
+	client2 := hub.Register(conn2)
+
+	broadcastSend := make(chan HubBroadcastMessageEvent, BufferSize)
+
+	broadcastSend <- HubBroadcastMessageEvent{
+		Message: []byte("test"),
+	}
+
+	// Close the channel to avoid a deadlock
+	close(broadcastSend)
+
+	hub.ProcessBroadcastMessages(broadcastSend)
+
+	time.Sleep(10 * time.Millisecond)
+
+	message := <-client.send
+
+	if string(message) != "test" {
+		t.Errorf("Expected Client One message to be 'test', got %v", message)
+	}
+
+	message = nil
+
+	message = <-client2.send
+
+	if string(message) != "test" {
+		t.Errorf("Expected Client Two message to be 'test', got %v", message)
+	}
+}
